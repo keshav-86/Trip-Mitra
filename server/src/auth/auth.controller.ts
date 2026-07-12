3fce54d7f79de288599966708f286ca90c319650
_keshav.86 <vkeshav7351@gmail.com>
2026-07-12 14:14:38 +0530

    feat(auth): complete JWT authentication system

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import { registerUser, loginUser, generateToken } from "./auth.service";
+import User from "../models/User";
+import { AuthRequest } from "../middleware/auth.middleware";
+import {
+  registerUser,
+  loginUser,
+  generateToken,
+} from "./auth.service";
 
+// Register Controller
 export const register = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req);
@@ -35,6 +42,7 @@ export const register = async (req: Request, res: Response) => {
   }
 };
 
+// Login Controller
 export const login = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req);
@@ -52,21 +60,45 @@ export const login = async (req: Request, res: Response) => {
 
     const token = generateToken(user._id.toString());
 
-return res.status(200).json({
-  success: true,
-  message: "Login successful",
-  token,
-  data: {
-    id: user._id,
-    fullName: user.fullName,
-    email: user.email,
-  },
-});
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      data: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
   } catch (error) {
     return res.status(400).json({
       success: false,
       message:
         error instanceof Error ? error.message : "Something went wrong",
+    });
+  }
+};
+
+// Profile Controller
+export const profile = async (req: AuthRequest, res: Response) => {
+  try {
+    const user = await User.findById(req.user?.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
 };
--- server/src/auth/auth.routes.ts
+++ server/src/auth/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register, login } from "./auth.controller";
+import { register, login, profile } from "./auth.controller";
 import { registerValidation } from "./auth.validation";
 import { loginValidation } from "./login.validation";
+import { protect } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/register", registerValidation, register);
 router.post("/login", loginValidation, login);
+router.get("/profile", protect, profile);
 
 export default router;
